6dbf590bd3dae1510808fce31290aff6bb3e68c0
danswart <danswartcpa@gmail.com>
2025-11-29 10:29:03 -0600

    update plot theming defaults

--- 1-Standalone-WORKS-scuc-tapr-data IChart Untrended Nonreactive No p or R2 namespaced.R
+++ 1-Standalone-WORKS-scuc-tapr-data IChart Untrended Nonreactive No p or R2 namespaced.R
@@ -2,37 +2,137 @@
 
 # # Load Libraries - NAMESPACED - NOT NEEDED
 
+# Force dplyr's select to take precedence
+select <- dplyr::select
+filter <- dplyr::filter
+
 # Options
 options(scipen = 999)
 options(qic.clshade = T) # NO LONGER NEEDED; CHARTS ALL PREPARED WITH GGPLOT2 ONLY
 options(qic.linecol = 'black') # NO LONGER NEEDED; CHARTS ALL PREPARED WITH GGPLOT2 ONLY
-options(qic.signalcol = "red") # NO LONGER NEEDED; CHARTS ALL PREPARED WITH GGPLOT2 ONLY
+options(qic.signalcol = "firebrick") # NO LONGER NEEDED; CHARTS ALL PREPARED WITH GGPLOT2 ONLY
 options(qic.targetcol = "purple") # NO LONGER NEEDED; CHARTS ALL PREPARED WITH GGPLOT2 ONLY
 options(DT.options = list(dom = 'pBlfrti')) # Add buttons, filtering, and top (t) pagination controls
 options(shiny.maxRequestSize = 50 * 1024^2) # Set upload maximum to 50 MB
+options(tigris_use_cache = TRUE)
+
+
+flextable::set_flextable_defaults(
+  font.size = 14,
+  font.family = "Cabin",
+  font.color = "black",
+  table.layout = "fixed",
+  border.color = "darkgray",
+  padding.top = 3,
+  padding.bottom = 3,
+  padding.left = 4,
+  padding.right = 4,
+  line_spacing = 1.3,
+  digits = 2,
+  decimal.mark = ",",
+  big.mark = " ",
+  na_str = "<na>"
+  # flextable::theme_alafoli()	|>  # BLAH
+  # flextable::theme_apa()  # THIS IS NICE
+  # flextable::theme_booktabs() |>  # NICE, MORE COMPACT
+  # flextable::theme_box() |>   # OK, INCLUDES CELL BORDERS
+  # flextable::theme_tron() |>  # 'DARK MODE' BLUE TEXT
+  # flextable::theme_tron_legacy() |>   # 'DARK MODE' YELLOW TEXT
+  # flextable::theme_vader() |>    # 'DARK MODE' WHITE TEXT
+  # flextable::theme_vanilla() |>   # NOT SPECIAL
+  # flextable::theme_zebra()	|>
+)
 
 
 # Set global theme for consistent plots
 ggplot2::theme_set(
   ggplot2::theme_minimal(base_size = 20) +
     ggplot2::theme(
-      plot.title = ggplot2::element_text(face = "bold", size = 26),
-      plot.subtitle = ggplot2::element_text(face = "bold", size = 24),
-      axis.title.x = ggplot2::element_text(face = "bold", size = 22),
-      axis.title.y = ggplot2::element_text(face = "bold", size = 22),
-      axis.text.x = ggplot2::element_text(
+      plot.title.position = "plot",
+      plot.title = ggtext::element_textbox_simple(
+        family = "Cabin",
         face = "bold",
+        color = "darkgreen",
         size = 22,
+        fill = "yellow",
+        lineheight = 1.2,
+        padding = ggplot2::margin(5.5, 5.5, 0.0, 5.5),
+        margin = ggplot2::margin(0, 0, 5.5, 0)
+      ),
+      plot.subtitle = ggtext::element_textbox_simple(
+        family = "Cabin",
+        color = "darkgreen",
+        face = "bold",
+        size = 20,
+        fill = "yellow",
+        lineheight = 1.2,
+        padding = ggplot2::margin(5.5, 5.5, 5.5, 5.5),
+        margin = ggplot2::margin(10.5, 0, 5.5, 0)
+      ),
+      plot.caption = ggtext::element_markdown(
+        family = "Cabin",
+        size = 16,
+        hjust = 1,
+        color = "darkblue",
+        face = "italic",
+        fill = "yellow",
+        lineheight = 1.0
+      ),
+      axis.text.x = ggtext::element_markdown(
+        family = "Cabin",
+        face = "bold",
+        color = "blue",
+        size = 16,
         angle = 45,
         hjust = 1
       ),
-      strip.text = ggplot2::element_text(face = "bold"),
-      legend.position = "bottom",
+      # ggplot2::element_blank(),
+      axis.title.x = ggtext::element_markdown(
+        family = "Cabin",
+        face = "bold",
+        color = "blue",
+        size = 16
+      ),
+      # ggplot2::element_blank(),
+      axis.text.y = ggtext::element_markdown(
+        family = "Cabin",
+        face = "bold",
+        color = "blue",
+        size = 16,
+        angle = 45,
+        hjust = 1
+      ),
+      # ggplot2::element_blank(),
+      axis.title.y = ggtext::element_markdown(
+        family = "Cabin",
+        face = "bold",
+        color = "blue",
+        size = 16
+      ),
+      # ggplot2::element_blank(),
+      strip.text = ggtext::element_markdown(
+        family = "Cabin",
+        color = "black",
+        size = ggplot2::rel(1.1),
+        face = "italic",
+        margin = ggplot2::margin(2, 0, 0.5, 0, "lines")
+      ),
+      axis.text = ggtext::element_markdown(
+        family = "Cabin",
+        color = "black"
+      ),
+      panel.background = ggplot2::element_rect(fill = "white", color = NA),
+      plot.background = ggplot2::element_rect(fill = "white", color = NA),
+      legend.position = "none",
       panel.spacing.x = grid::unit(1.5, "cm"),
       panel.spacing.y = grid::unit(1.5, "cm"),
       plot.margin = ggplot2::margin(20, 20, 20, 20, "pt")
     )
 )
+
+
+# Set seed for reproducibility
+base::set.seed(123)
 
 ########  CONVERTING AN UPDATED VERSION OF THE CSV FILE IN WIDE FORMAT TO LONG FORMAT ##########
 
